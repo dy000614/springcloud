f1c94e0cb6d1d310c5320a24a8fd4bd6687bbf2e
asus <626997061@qq.com>
2022-02-21 14:46:28 +0800

    springcloud1

---
+++ sp09-config/src/main/java/cn/tedu/sp09/Sp09ConfigApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp09ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp09ConfigApplication.class, args);
+    }
+
+}
